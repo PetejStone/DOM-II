d4c6e690c9d20bdea91501401ea0c545e58954ac
PetejStone <petejstoney@gmail.com>
2019-04-09 14:56:12 -0700

    added form and submit

--- js/index.js
+++ js/index.js
@@ -1,25 +1,20 @@
 // Your code goes here
 const navItem = Array.from(document.querySelector('nav').children);
 const hide = document.querySelectorAll('.hide');
+const imgList = Array.from(document.querySelectorAll('img'));
+const submit = document.querySelector('#submit');
+const overlay = document.querySelector('.overlay');
+const button = Array.from(document.querySelectorAll('.btn'));
+const cancel = document.querySelector('#cancel');
+const form = document.querySelector('form');
 
 
-// navItem.forEach(item => item.addEventListener('click',()=>{
-//   item.insertAdjacentHTML('beforeend','<div class="selected "></div>');
-//   const selected = document.querySelector('.selected');
-//   console.log(selected);
-//     if (selected.style.opacity = 1) {
-//       selected.style.opacity = 0;
-//       console.log('test');
-//     } else if (selected.style.opacity = 1){
-//       selected.style.opacity = 0;
-//       console.log('hello');
-//     }
-// }));
-
+///1. click event listener
 //Navigation 'active' functionality.
 for (let i=0; i < navItem.length; i++) {
   let activeIcon = navItem[i].nextElementSibling; //selects active icon
   navItem[i].addEventListener('click',()=> { //adds click listener to nav links
+
     for (let i=0; i < hide.length; i ++) { //loops through all icons and turns their active stylings 'off'
       hide[i].className = 'hide';
       navItem[i].className = 'navItem';
@@ -29,4 +24,42 @@ for (let i=0; i < navItem.length; i++) {
     event.target.className = 'active-link';
 
   });
+
 }
+//////
+
+///2. mouseover event listener
+
+imgList.forEach(item => item.addEventListener('mouseover',()=>{
+   item.style.transform = "scale(1.01)";
+   item.style.cursor = 'pointer';
+}));
+//////
+
+////3. mouseleave event listenr
+imgList.forEach(item => item.addEventListener('mouseleave',()=>{
+   item.style.transform = "scale(1)";
+}));
+
+/////
+
+
+//form submit event listener
+
+//Form button functionilty
+//4. submit event listener
+form.addEventListener('submit', (e)=> {
+  e.preventDefault();
+  alert('We Will Contact You Shortly!');
+  overlay.style.display = 'none';
+});
+
+button.forEach(btn => addEventListener('click', (e)=> {
+  overlay.style.display = 'flex';
+
+
+}));
+
+cancel.addEventListener('click', (e)=> {
+  overlay.style.display = 'none';
+});
